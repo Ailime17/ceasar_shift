a1324f7970ae09524578cbd26841563d6a307ce7
Emilia <emilia.skrzypczak.es@gmail.com>
2022-12-09 13:59:09 +0100

    Add code to improve functionality & refactor code

--- script.rb
+++ script.rb
@@ -1,5 +1,7 @@
-def caesar_cipher(string, shift_factor)
-  
+def caesar_cipher(string, shift_factor) 
+  # assign the original string to a variable to use later
+  og_string = string
+
   if shift_factor > 32 || shift_factor < 0 || string == ""
     return
   else
@@ -11,14 +13,12 @@ def caesar_cipher(string, shift_factor)
       my_hash[letter] = letter_nr
     end
 
-    
     # transform the string into an array of lowercase letters
     string_ar = string.downcase.split('')
-    
-    # transform an array of letters into an array with hashes conisting of 
-    # letters from the string as keys and corresponding numbers as values
+
+    # transform an array of letters into an array with hashes consisting of letters from the string as keys and corresponding numbers as values
     transformed_string_ar = string_ar.map do |letter|
-      my_hash.select do |key, value|
+      my_hash.select do |key, _value|
         letter == key
       end
     end
@@ -26,27 +26,26 @@ def caesar_cipher(string, shift_factor)
     # create an array with only the corresponding numbers
     numbers_ar = []
     transformed_string_ar.each do |item|
-      item.each do |key, value|
+      item.each do |_key, value|
         numbers_ar.push(value)
       end
     end
 
-    # increment the numbers by the shift_factor 
-     new_numbers_ar = numbers_ar.map do |number|
+    # increment the numbers by the shift_factor
+    new_numbers_ar = numbers_ar.map do |number|
       number += shift_factor
     end
     final_numbers_ar = new_numbers_ar.map do |n|
       if n > 26
-        n = n - 26
+        n -= 26
       else
         n
       end
     end
-    
-    # create an array with hashes with the numbers as values and the 
-    # corresponding letters as keys  
+
+    # create an array with hashes with the numbers as values and the corresponding letters as keys
     transformed_numbers_ar = final_numbers_ar.map do |number|
-      my_hash.select do |key,value|
+      my_hash.select do |_key,value|
         number == value
       end
     end
@@ -64,37 +63,42 @@ def caesar_cipher(string, shift_factor)
     # check if original string has any !?,.-/ signs and if yes, add them to the final string
     signs_array = ["!", "?", ",", ".", "-", "/"]
     while (string.join.count "!?,.-/") != (final_transformed_letters.join.count "!?,.-/") do
-    signs_array.each do |sign|
-      if string.index(sign) != nil
-         string.each_with_index do |v,i|
-           if v == sign
-             final_transformed_letters.insert(i, sign)
-           end
+      signs_array.each do |sign|
+        next if string.index(sign).nil?
+
+        string.each_with_index do |v,i|
+          if v == sign
+            final_transformed_letters.insert(i, sign)
           end
-           
+        end
       end
-    end
     end
     final_transformed_letters = final_transformed_letters.compact
 
     # check if original string has any empty spaces and if yes, add them to the final string
-  while (string.join.count " ") != (final_transformed_letters.join.count " ") do
-    
-    if string.index(" ") != nil
-       string.each_with_index do |v,i|
-         if v == " "
-           final_transformed_letters.insert(i, " ")
-         end
-        end
-         
-    end
-  end
-  
+    while (string.join.count " ") != (final_transformed_letters.join.count " ") do
+      next if string.index(" ").nil?
 
-    # transform the array of letters into a string and return it
+      string.each_with_index do |v,i|
+        if v == " "
+          final_transformed_letters.insert(i, " ")
+        end
+      end
+    end
+
+    # transform the array of letters into a string
     final_string = final_transformed_letters.join
+
+    # switch letters that need to be uppercase to uppercase
+    0.upto(og_string.length-1) do |n|
+      if og_string[n] == og_string[n].upcase
+        final_string[n] = final_string[n].upcase
+      end
+    end
+
+    # return the final string
     p final_string
     return final_string
   end
 end
- caesar_cipher("hello!?How r u...?",5)
+caesar_cipher('hello!?How r u...?',5)
